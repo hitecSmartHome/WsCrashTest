2314472a34018368d360ad4a845bdd8167c3c916
zekageri <88187809+hitecSmartHome@users.noreply.github.com>
2024-07-10 15:11:59 +0200

    Example

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,27 @@ void setup() {
   Serial.begin(115200);
 
   webSocket.onConnect([]() {
-    Serial.println("Connected");
+    Serial.println("WS - Connected");
   });
 
   webSocket.onDisconnect([]() {
-    Serial.println("Disconnected");
+    Serial.println("WS - Disconnected");
   });
 
   webSocket.onFrame([](char* data, size_t length) {
-    Serial.println(data);
+    Serial.printf("WS - Received: %s\n", data);
   });
 
   webSocket.onError([](WsError error) {
-    Serial.println(error.message);
+    Serial.printf("WS - Error: %d. Message: %s\n", error.code, error.message);
   });
 
   webSocket.listen(SERVER_URL);
+  // webSocket.listen(SERVER_URL, "/path");
+  // webSocket.listen(IPAddress(192, 168, 0, 1), 80, "/path");
+  // webSocket.listen(IPAddress(192, 168, 0, 1), 80);
 }
 
 void loop() {
-  
+  vTaskDelay(1);
 }
